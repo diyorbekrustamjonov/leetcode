ac286c222c9bd5369e47c6a48e89990944f54ee9
Diyorbek Rustamjonov <bdvhattab@gmail.com>
2024-01-06 01:13:39 +0500

    feat: added 2665 problem solutions

---
+++ 30_day_challenge/03_2665_counter_ii.ts
@@ -0,0 +1,42 @@
+// Interface of Counter
+type Counter = {
+    increment: () => number,
+    decrement: () => number,
+    reset: () => number,
+}
+
+//1. Solution
+function createCounter(init: number): Counter {
+    let counter = init
+    return {
+        increment: (): number => ++counter,
+        decrement: (): number => --counter,
+        reset: (): number => {
+            counter = init
+            return init;
+        },
+    }
+};
+
+//2. Solution
+function createCounter2(init: number): Counter {
+    let counter = init;
+    function increment(): ReturnType<Counter["increment"]> {
+        return ++counter;
+    }
+
+    function decrement(): ReturnType<Counter["decrement"]> {
+        return --counter;
+    }
+
+    function reset(): ReturnType<Counter["reset"]> {
+        counter = init
+        return counter;
+    }
+
+    return {
+        increment,
+        decrement,
+        reset
+    }
+};
