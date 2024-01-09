97d962466963cfc4df30d2040c15dde43c542f26
Diyorbek Rustamjonov <bdvhattab@gmail.com>
2024-01-09 22:30:06 +0500

    feat: added 2634 problem solution

--- 30_day_challenge/05_2634_filter_elements_from_array.ts
+++ 30_day_challenge/05_2634_filter_elements_from_array.ts
@@ -10,3 +10,16 @@ function filter(arr: number[], fn: Fn): number[] {
   });
   return newArr;
 };
+
+// 2. Solution
+function filter2(arr: number[], fn: Fn): number[] {
+    const filteredArray : number[] = [];
+
+    for(let i = 0; i < arr.length; i++)
+    {
+        if(fn(arr[i],i)){
+            filteredArray.push(arr[i]);
+        }
+    }
+    return filteredArray;
+};
