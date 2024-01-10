435bf175e403dec9206b5fb413d3e76d24c386d0
Diyorbek Rustamjonov <bdvhattab@gmail.com>
2024-01-10 23:43:43 +0500

    feat: added 2626 problem solution

--- 30_day_challenge/06_2626_array_reduce_transformation.ts
+++ 30_day_challenge/06_2626_array_reduce_transformation.ts
@@ -4,3 +4,12 @@ type Fn = (accum: number, curr: number) => number
 function reduce(nums: number[], fn: Fn, init: number): number {
     return nums.reduce((prev, curr) => fn(prev, curr), init)
 };
+
+// 2. Solution
+function reduce2(nums: number[], fn: Fn, init: number): number {
+    for(let i = 0; i < nums.length; i++) {
+        init = fn(init, nums[i])
+    }
+
+    return init;
+};
